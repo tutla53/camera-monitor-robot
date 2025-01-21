453201951131ae2d2955a277f231b78f51338133
Tutla Ayatullah <39817427+tutla53@users.noreply.github.com>
2025-01-21 17:51:47 +0700

    revert the therehsold

--- remote-control/src/tasks/control_task.rs
+++ remote-control/src/tasks/control_task.rs
@@ -42,10 +42,10 @@ pub async fn control_task(r: ControlResources) {
         let head_val = adc.read(&mut head_pin).await.unwrap();
         let body_val = adc.read(&mut body_pin).await.unwrap();
 
-        if head_val > 4000 { uart_tx.write("w".as_bytes()).await.unwrap(); }
+        if head_val > 3500 { uart_tx.write("w".as_bytes()).await.unwrap(); }
         else if head_val < 500{ uart_tx.write("s".as_bytes()).await.unwrap(); }
 
-        if body_val > 4000 { uart_tx.write("a".as_bytes()).await.unwrap(); }
+        if body_val > 3500 { uart_tx.write("a".as_bytes()).await.unwrap(); }
         else if body_val < 500{ uart_tx.write("d".as_bytes()).await.unwrap(); }
 
         Timer::after_millis(200).await;
